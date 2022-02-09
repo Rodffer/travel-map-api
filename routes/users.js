aaa87054db7179a49a74df1161e9f3b83f9c5d07
Rayron Rodffer <r.rodffer@gmail.com>
2022-02-09 20:52:12 -0300

    Feat: add hash do pass e validacoes no cadastro do usuario

--- routes/users.js
+++ routes/users.js
@@ -1,19 +1,44 @@
 const router = require('express').Router();
 const User = require('../models/User');
+const bcrypt = require('bcrypt');
 
 // create user
 
 router.post('/', async (req, res) => {
   try {
-    const newUser = await new User(req.body);
+    const { userName, email } = req.body;
+
+    const existsUser = await User.exists({ userName });
+
+    if(existsUser){
+      return res.status(400).json({ message: 'Username indisponível'});
+    }
+
+    const existsEmail = await User.exists({ email });
+
+    if(existsEmail){
+      return res.status(400).json({ message: 'E-mail indisponível'});
+    }
+
+    const hashedPass = await bcrypt.hash(req.body.password, parseInt(process.env.BCRYPT_SALT));
+
+    const newUser = await new User({
+      userName,
+      email,
+      password: hashedPass
+    });
 
     const savedUser = await newUser.save();
 
-    return res.status(201).json(savedUser);
+    const { password, updatedAt, ...other } = savedUser._doc;
+
+    return res.status(201).json(other);
   } catch (error) {
     return res.status(500).json({ error: error});
   }
 });
+
+// login
 
 // get all user
 
